38ba4b90726c08ccac1e4bf275e8da636287d5f5
Directrix777 <andrew.j.ribas@gmail.com>
2020-11-24 13:01:49 -0600

    Added custom routing to match the custom actions

--- fomopoly-backend/config/routes.rb
+++ fomopoly-backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  patch '/users/transact', to: 'users#transact'
+  patch '/users/pay-bank', to: 'users#pay_bank'
+  patch '/users/move-a-space', to: 'users#move_a_space'
   resources :spaces
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
