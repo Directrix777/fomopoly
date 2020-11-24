78f99dabe33965d43e7c75f91e68bc178412abdb
Directrix777 <andrew.j.ribas@gmail.com>
2020-11-23 21:48:43 -0600

    Changed migration to allow number of houses to be managed by frontend

--- fomopoly-backend/db/migrate/20201124010056_create_spaces.rb
+++ fomopoly-backend/db/migrate/20201124010056_create_spaces.rb
@@ -2,7 +2,6 @@ class CreateSpaces < ActiveRecord::Migration[6.0]
   def change
     create_table :spaces do |t|
       t.string :color
-      t.integer :houses
       t.string :name
       t.integer :flat_rent
       t.integer :one_house_rent
