18240a48363afffabd285ed0e11c6151407fab46
Directrix777 <andrew.j.ribas@gmail.com>
2020-11-24 19:57:19 -0600

    ran migration

--- fomopoly-backend/db/schema.rb
+++ fomopoly-backend/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_165457) do
+ActiveRecord::Schema.define(version: 2020_11_24_193513) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "host_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "spaces", force: :cascade do |t|
     t.string "color"
