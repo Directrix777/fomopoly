0b458ea2b318f6d62e8cf179e6539432cfc2c34e
Directrix777 <andrew.j.ribas@gmail.com>
2020-12-01 00:14:08 -0600

    Added move user action in reducer

--- fomopoly-frontend/src/reducers/gameReducer.js
+++ fomopoly-frontend/src/reducers/gameReducer.js
@@ -11,13 +11,31 @@ const gameReducer = (state = {spaces: [], users: [], loading: false}, action) =>
             return {...state, users: action.users, loading: false}
         case 'ADD_USER':
             return {...state, users: [...state.users, action.user]}
-        case 'DELETE_USER':
-            return {...state, users: state.users.map((user) => {
-                if(user.id !== action.id)
+        case 'MOVE_USER':
+            let newUsers = state.users.map((user) => {
+                if(user.id === action.id)
                 {
-                    return user
+                    console.log("Found user to update location")
+                    let newUser = {}
+                    Object.assign(newUser, user)
+                    console.log(newUser)
+                    if(user.current_location === 40)
+                    {
+                        newUser.current_location = 1
+                        console.log(newUser)
+                        return newUser
+                    }
+                    else
+                    {
+                        newUser.current_location += 1
+                        return newUser
+                    }
                 }
-            })}
+                return user
+            })
+            return {...state, users: newUsers}
+        case 'DELETE_USER':
+            return {...state, users: state.users.filter((user) => {return user.id !== action.id})}
         default: 
             return state
     }
