a5d344dc199fcbbc950f620ab7eb4e3eff1822b6
Directrix777 <andrew.j.ribas@gmail.com>
2021-01-22 10:50:10 -0600

    Added action for selling a space to a user

--- fomopoly-frontend/src/actions/spaceActions.js
+++ fomopoly-frontend/src/actions/spaceActions.js
@@ -6,3 +6,19 @@ export const fetchSpaces = () => {
         .then(r => {dispatch({type: 'ADD_SPACES', spaces: r})})
     }
 }
+
+export const sellSpace = (space, user) => { //both parameters are integers corresponding to an id value
+    return (dispatch) => {
+        const options = {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({user: user})
+        }
+        fetch(`http://localhost:3000/spaces/${space}`)
+        .then(r => r.json())
+        .then(r => console.log(r))
+    }
+}
