e5157f3e575189737a770793301943e44f337570
Directrix777 <andrew.j.ribas@gmail.com>
2021-01-22 10:59:25 -0600

    Moved rendering of prop card into a contitional statement Added places to render railroad and utility cards

--- fomopoly-frontend/src/components/PropCard.js
+++ fomopoly-frontend/src/components/PropCard.js
@@ -12,27 +12,45 @@ export default function PropCard(props) {
         }
         return words.join(' ').toUpperCase()
     }
-
-    return(
-        <div className='prop-card'>
-            <div className={`property-color-${props.space.color}`}>
-                <p className='title-deed' style={{fontSize: '6pt', letterSpacing: '2px'}}>{'TITLE DEED'}</p>
-                <p className='property-name' style={{fontSize: '8.6pt'}}>{abbreviateAvenue(props.space.name)}</p>
-            </div>
-            <p className='rent-line'>{`RENT: ₣${props.space.flat_rent}`}</p>
-            <p className='house-rent-line'>{`With 1 House ₣${props.space.one_house_rent}`}</p>
-            <p className='house-rent-line'>{`With 2 Houses ₣${props.space.two_house_rent}`}</p>
-            <p className='house-rent-line'>{`With 3 Houses ₣${props.space.three_house_rent}`}</p>
-            <p className='house-rent-line'>{`With 4 Houses ₣${props.space.four_house_rent}`}</p>
-            <p className='rent-line'>{`With HOTEL ₣${props.space.four_house_rent}`}</p>
-            <hr className='prop-divider'/>
-            <p className='rent-line'>{`Mortgage Value ₣${props.space.mortgage_value}`}</p>
-            <p className='rent-line'>{`Houses cost ₣${props.space.house_price}`}</p>
-            <p className='rent-line'>{`Hotels cost ₣${props.space.house_price}`}</p>
-            <p className='rent-line' style={{fontSize: '7pt'}}>{`(plus 4 houses)`}</p>
-
-        </div>
-    )
+    if(props.space.color)
+    {
+        if (props.space.color === 'Black')
+        {
+            return (
+                <div className='prop-card'></div>
+            )
+        }
+        else if (props.space.color === 'Mint')
+        {
+            return (
+                <div className='prop-card'></div>
+            )
+        }
+        else
+        {
+            return(
+                <div className='prop-card'>
+                    <div className={`property-color-${props.space.color}`}>
+                        <p className='title-deed' style={{fontSize: '6pt', letterSpacing: '2px'}}>{'TITLE DEED'}</p>
+                        <p className='property-name' style={{fontSize: '8.6pt'}}>{abbreviateAvenue(props.space.name)}</p>
+                    </div>
+                    <p className='rent-line'>{`RENT: ₣${props.space.flat_rent}`}</p>
+                    <p className='house-rent-line'>{`With 1 House ₣${props.space.one_house_rent}`}</p>
+                    <p className='house-rent-line'>{`With 2 Houses ₣${props.space.two_house_rent}`}</p>
+                    <p className='house-rent-line'>{`With 3 Houses ₣${props.space.three_house_rent}`}</p>
+                    <p className='house-rent-line'>{`With 4 Houses ₣${props.space.four_house_rent}`}</p>
+                    <p className='rent-line'>{`With HOTEL ₣${props.space.four_house_rent}`}</p>
+                    <hr className='prop-divider'/>
+                    <p className='rent-line'>{`Mortgage Value ₣${props.space.mortgage_value}`}</p>
+                    <p className='rent-line'>{`Houses cost ₣${props.space.house_price}`}</p>
+                    <p className='rent-line'>{`Hotels cost ₣${props.space.house_price}`}</p>
+                    <p className='rent-line' style={{fontSize: '7pt'}}>{`(plus 4 houses)`}</p>
+        
+                </div>
+            )
+        }
+    }
+    
     // color: "", ding
     // name: "", ding
     // flat_rent: 0, ding
